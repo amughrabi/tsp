4e7e6b48469fb6d836f04376774fc6c08f9ee9e7
amughrabi <ahmad.al_mughrabi@live.com>
2018-10-23 01:58:26 +0300

    HCA

--- src/main/java/jo/ju/edu/tsp/algorithms/HCA.java
+++ src/main/java/jo/ju/edu/tsp/algorithms/HCA.java
@@ -3,8 +3,12 @@ package jo.ju.edu.tsp.algorithms;
 import jo.ju.edu.tsp.core.Graph;
 import jo.ju.edu.tsp.core.Vertex;
 import org.jetbrains.annotations.NotNull;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -14,38 +18,45 @@ public class HCA extends TSP {
     private double temperature = 50;
 
     public @NotNull Graph solve(@NotNull Graph graph) {
-        // distribute the initial amount of soil and depth on the vertices.
-        initParams(graph);
+        // Maximum number of iterations is a triple the number of vertices
+        maxIterations = 3 * graph.getNumberOfVertices();
 
         // A graph of water drops solutions, each index represent a water drop.
         Graph solutions = new Graph(graph.getNumberOfVertices(), "Solutions");
         // Number of water drops equals to number of vertices
-        List<WaterDrop> waterDrops = getWDs(graph);
+        List<WaterDrop> waterDrops;
         WaterDrop waterDrop;
         double gDepth, fSoil, probability, sum, theChosenOneValue;
         Vertex theChosenOne;
+        HashMap<Integer, List<Vertex>> bestGlobalSolution = null;
+        int cycle = 0;
         while (temperature < maxTemperature) {
+            // distribute the initial amount of soil and depth on the vertices.
+            initParams(graph, bestGlobalSolution);
+            waterDrops = getWDs(graph);
             for(int iteration = 0; iteration < maxIterations; iteration++) {
                 for(int i = 0; i < waterDrops.size(); i++) {
                     waterDrop = waterDrops.get(i);
                     theChosenOne = null;
                     theChosenOneValue = 0;
                     for(Vertex vertex : graph.adjacentOf(waterDrop.getCurrentVertexId())) {
-                        gDepth = gDepth(vertex);
-                        fSoil = fSoil(vertex);
-                        sum = 0;
-                        if(solutions.adjacentOf(i) == null) {
-                            sum = Math.pow(fSoil, 2) * gDepth;
-                        } else {
-                            for(Vertex vc : solutions.adjacentOf(i)) {
-                                sum += (Math.pow(fSoil(vc), 2) * gDepth(vc));
+                        if(!waterDrop.isVisited(vertex.getId())) {
+                            gDepth = gDepth(vertex);
+                            fSoil = fSoil(vertex);
+                            sum = 0;
+                            if(solutions.adjacentOf(i) == null || solutions.adjacentOf(i).isEmpty()) {
+                                sum = Math.pow(fSoil, 2) * gDepth;
+                            } else {
+                                for(Vertex vc : solutions.adjacentOf(i)) {
+                                    sum += (Math.pow(fSoil(vc), 2) * gDepth(vc));
+                                }
                             }
-                        }
 
-                        probability = (Math.pow(fSoil, 2) * gDepth) / sum;
-                        if(theChosenOneValue < probability) {
-                            theChosenOneValue = probability;
-                            theChosenOne = vertex;
+                            probability = (Math.pow(fSoil, 2) * gDepth) / sum;
+                            if(theChosenOneValue < probability) {
+                                theChosenOneValue = probability;
+                                theChosenOne = vertex;
+                            }
                         }
                     }
                     // Mark the current vertex as visited
@@ -67,24 +78,80 @@ public class HCA extends TSP {
             // 3.4. Evaporation Stage
             List<WaterDrop> evaporatedWaterDrops = evaporation(waterDrops);
             // 3.5. Condensation Stage
-            List<WaterDrop> condensationWaterDrops = condensation(evaporatedWaterDrops, solutions);
+            WaterDrop theCollector = condensation(evaporatedWaterDrops, solutions);
+            // 3.6. Precipitation Stage
+            if(theCollector != null) {
+                bestGlobalSolution = new HashMap<Integer, List<Vertex>>();
+                bestGlobalSolution.put(theCollector.getWaterDropId(), solutions.adjacentOf(theCollector.getWaterDropId()));
+            }
 
+            cycle ++;
         }
-
-        return null;
-    }
-
-    private List<WaterDrop> condensation(List<WaterDrop> wds, Graph solutions) {
-        double similarity;
-        for(int i = 1; i < wds.size(); i++) {
-            similarity = similarity(solutions.adjacentOf(i - 1), solutions.adjacentOf(i));
-            if(similarity < 50) { // Bounce
-
-            } else { // Merge
-
+        if(bestGlobalSolution != null) {
+            for(int i : bestGlobalSolution.keySet()) {
+                for(Vertex v : bestGlobalSolution.get(i)) {
+                    System.out.print("  ->  " + v.getId());
+                }
             }
         }
-        return null;
+        return new Graph(0, "");
+    }
+
+    private WaterDrop condensation(List<WaterDrop> wds, Graph solutions) {
+        double similarity;
+        for(int i = 0; i < wds.size(); i++) {
+            if(wds.get(i) != null) {
+                for(int j = 0; j < wds.size(); j++) {
+                    if(wds.get(j) != null && isCollideWaterDrops(solutions.adjacentOf(i), solutions.adjacentOf(j))) {
+                        similarity = similarity(solutions.adjacentOf(i), solutions.adjacentOf(j));
+                        if(similarity >= 50) {// Merge
+                            // When two water drops collide and merge, one water drop (i.e., the collector) will
+                            // become more powerful by eliminating the other one in the process also acquiring the
+                            // characteristics of the eliminated water drop (i.e., its velocity)
+                            WaterDrop wd1 = wds.get(i); // the collector
+                            WaterDrop wd2 = wds.get(j);
+                            wd1.setVelocity(wd1.getVelocity() + wd2.getVelocity());
+                            wds.set(j, null); // eliminating
+                        } else {
+                            // Bounce - for now, leave it empty. The weight is NOT used in Node selection in the
+                            // flow stage. We only depends on the soil, velocity, carried soil, and temperature
+                        }
+                    }
+                }
+            }
+        }
+        // In addition, at this stage, the temperature is reduced by 50°C. The lowering and raising of the temperature
+        // helps to prevent the water drops from sticking with the same solution every iteration.
+        temperature -= 50;
+
+        return findTheCollector(wds);
+    }
+
+    // Higher velocity, higher probability to be the collector.
+    private WaterDrop findTheCollector(List<WaterDrop> wds) {
+        double max = -1;
+        WaterDrop collector = null;
+        for (WaterDrop wd : wds) {
+            if (wd != null && max < wd.getVelocity()) {
+                max = wd.getVelocity();
+                collector = wd;
+            }
+        }
+        return collector;
+    }
+
+    private boolean isCollideWaterDrops(List<Vertex> solution1, List<Vertex> solution2) {
+        return solution1.size() == solution2.size();
+    }
+
+    /* NOT USED FOR NOW */
+    private HashMap<Integer, Double> calcOccurrences(List<Vertex> solution1, List<Vertex> solution2) {
+        HashMap<Integer, Double> occurrences = new HashMap<Integer, Double>(solution1.size());
+        for(int i = 0; i < solution1.size(); i++) {
+            occurrences.put(solution1.get(i).getId(), occurrences.get(solution1.get(i).getId()) + 1);
+            occurrences.put(solution2.get(i).getId(), occurrences.get(solution2.get(i).getId()) + 1);
+        }
+        return occurrences;
     }
 
     private List<WaterDrop> evaporation(List<WaterDrop> wds) {
@@ -174,25 +241,38 @@ public class HCA extends TSP {
 
     private List<WaterDrop> getWDs(Graph graph) {
         List<WaterDrop> waterDrops = new ArrayList<WaterDrop>(graph.getNumberOfVertices());
-        for(int i = 0; i < waterDrops.size(); i++) {
+        for(int i = 0; i < graph.getNumberOfVertices(); i++) {
             // Starting point is the same water drop ID.
             waterDrops.add(i, new WaterDrop(i, i, waterDrops.size()));
         }
         return waterDrops;
     }
-
-    private void initParams(@NotNull Graph graph) {
-        // Maximum number of iterations is a triple the number of vertices
-        maxIterations = 3 * graph.getNumberOfVertices();
-
+    // reinitializing all the dynamic variables, such as the amount of the soil on each edge, depth of paths,
+    // the velocity of each water drop, and the amount of soil it holds.
+    private void initParams(@NotNull Graph graph, HashMap<Integer, List<Vertex>> bestGlobalSolution) {
         List<Vertex> vertices;
+        double soil;
         for(int i = 0; i < graph.getNumberOfVertices(); i++) {
             vertices = graph.adjacentOf(i);
             for(Vertex vertex : vertices) {
-                vertex.addCharacteristic("soil", initSoil);
-                vertex.addCharacteristic("depth", (vertex.getCost() / initSoil) );
+                soil = initSoil;
+                if(bestGlobalSolution != null && bestGlobalSolution.get(i) != null) {
+                    if(isBelong(vertex, bestGlobalSolution.get(i))) {
+                        soil = 0.9 * initSoil;
+                    }
+                }
+                vertex.addCharacteristic("soil", soil);
+                vertex.addCharacteristic("depth", (vertex.getCost() / soil) );
             }
         }
+    }
+
+    private boolean isBelong(Vertex v, List<Vertex> vs) {
+        for(Vertex vertex : vs) {
+            if(vertex.getId() == v.getId())
+                return true;
+        }
+        return false;
     }
 
     /*
@@ -236,5 +316,18 @@ public class HCA extends TSP {
             if(wd1.get(i).getId() != wd2.get(i).getId()) counter++;
         }
         return (counter / wd1.size()) * 100; // output in percentage. 50%
+    }
+
+    public static void main(String[] args) {
+        TSP tsp = new HCA();
+        try {
+            tsp.solve();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
